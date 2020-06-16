1e714adb1f779fa97683f0c5407d22858f3874f0
Joshua May <notjosh@gmail.com>
2020-06-16 10:41:41 +0200

    Import static site

---
+++ hello.js
@@ -0,0 +1,74 @@
+// a key map of allowed keys
+var allowedKeys = {
+  72: "h",
+  69: "e",
+  76: "l",
+  79: "o",
+};
+
+// the sequence
+var nohelloCode = ["h", "e", "l", "l", "o"];
+
+// a variable to remember the 'position' the user has reached so far.
+var nohelloCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener("keydown", function (e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the code
+  var requiredKey = nohelloCode[nohelloCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+    // move to the next key in the  sequence
+    nohelloCodePosition++;
+
+    // if the last key is reached, activate wut
+    if (nohelloCodePosition == nohelloCode.length) {
+      activateWut();
+      nohelloCodePosition = 0;
+    }
+  } else {
+    nohelloCodePosition = 0;
+  }
+});
+
+function activateWut() {
+  document.body.style.backgroundImage = "url('img/why.gif')";
+
+  document.getElementById("wholesite").style.cssText = "color:white !important";
+
+  document.getElementsByTagName("p")[0].style.cssText =
+    "color:white !important";
+  document.getElementsByTagName("p")[1].style.cssText =
+    "color:white !important";
+  document.getElementsByTagName("p")[2].style.cssText =
+    "color:white !important";
+  document.getElementsByTagName("p")[3].style.cssText =
+    "color:white !important";
+  document.getElementsByTagName("p")[4].style.cssText =
+    "color:white !important";
+  document.getElementsByTagName("p")[5].style.cssText =
+    "color:white !important";
+  document.getElementsByTagName("p")[6].style.cssText =
+    "color:white !important";
+  document.getElementsByTagName("p")[7].style.cssText =
+    "color:white !important";
+
+  document.getElementsByTagName("ul")[0].style.cssText =
+    "color:white !important";
+  document.getElementsByTagName("ul")[1].style.cssText =
+    "color:white !important";
+
+  document.getElementsByTagName("footer")[0].style.cssText =
+    "background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.3) 45%) !important; color:white !important";
+
+  document.getElementsByClassName("one")[0].style.cssText =
+    "box-shadow: -20px 20px 60px #000000, 20px -20px 60px #000000 !important";
+  document.getElementsByClassName("two")[0].style.cssText =
+    "box-shadow: -20px 20px 60px #000000, 20px -20px 60px #000000 !important";
+
+  document.getElementsByClassName("subtitle")[0].style.cssText =
+    "color:white !important; opacity: 50%";
+}
---
+++ nohello.js
@@ -0,0 +1,58 @@
+var typed2 = new Typed("#strike", {
+  strings: [
+    "hello",
+    "hi",
+    "hey",
+    "g'day",
+    "bonjour",
+    "yo",
+    "hola",
+    "morning!",
+    "hallo",
+    "ciao",
+    "&#128075;",
+    "namaste",
+    "hoi",
+    "m'athchomaroon",
+    "hiya",
+    "Привет",
+    "you got a sec?",
+    "مرحبا",
+    "greetings!",
+    "aloha",
+    "こんにちは",
+    "buenos dias",
+    "nuqneH",
+    "heya",
+    "olà",
+    "apipoulaï",
+    "j0",
+    "howdy",
+    "שלום",
+    "yooooooooooo!",
+    "你好",
+    "you there?",
+    "fraeslis",
+    "여보세요",
+    "sul sul",
+    "quick question...",
+    "achuta",
+    "ਸਤ ਸ੍ਰੀ ਅਕਾਲ",
+    "ping",
+    "Χαίρετε",
+  ],
+  typeSpeed: 80,
+  backSpeed: 60,
+  smartBackspace: false,
+  loop: true,
+  shuffle: false,
+  backDelay: 2000,
+  startDelay: 3000,
+});
+
+// force the start of cursor animation while the `startDelay` is ticking
+if (typed2.cursor != null) {
+  // can't use `toggleBlinking(true)` here, as it has some extra checks
+  // whether animation has started...which defeats the purpose
+  typed2.cursor.classList.add("typed-cursor--blink");
+}
