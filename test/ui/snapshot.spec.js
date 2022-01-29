01a1c4e0245b61cd37aacfc4fdf84d312998a9fd
Joshua May <notjosh@gmail.com>
2022-01-29 23:29:01 +0100

    Forcing animation pauses in tests, fixes flakiness

--- test/ui/snapshot.spec.js
+++ test/ui/snapshot.spec.js
@@ -2,9 +2,25 @@ const { test, expect } = require('@playwright/test');
 
 const BASE_URL = 'http://localhost:8123/';
 
-test('index', async ({ page }) => {
-  await page.goto(BASE_URL);
-  const screenshot = await page.screenshot({ fullPage: true });
+test.describe('index', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(BASE_URL);
 
-  expect(screenshot).toMatchSnapshot('index.png', { threshold: 0.2 });
+    // pause cursor blinking
+    await page.$eval('.typed-cursor', (el) =>
+      el.classList.remove('typed-cursor--blink')
+    );
+  });
+
+  test('page looks the same', async ({ page }) => {
+    const screenshot = await page.screenshot({ fullPage: true });
+    expect(screenshot).toMatchSnapshot('index.png', { threshold: 0.2 });
+  });
+
+  test('why', async ({ page }) => {
+    await page.keyboard.type('hello');
+
+    const screenshot = await page.screenshot();
+    expect(screenshot).toMatchSnapshot('why.png', { threshold: 0.2 });
+  });
 });
