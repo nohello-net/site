bec12065365319b1644aa79c71e2b130c9f3e286
Joshua May <notjosh@gmail.com>
2022-01-30 19:12:11 +0100

    First implementation of _some_ localisation, but it...only sets up foundations

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,17 @@
+const i18n = require('eleventy-plugin-i18n-gettext');
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/img');
   eleventyConfig.addPassthroughCopy('src/js');
+
+  eleventyConfig.addPlugin(i18n, {
+    localesDirectory: 'locales',
+    parserMode: 'po',
+    javascriptMessages: 'messages.js',
+    tokenFilePatterns: ['src/**/*.njk', 'src/**/*.js'],
+    localeRegex: /^(?<lang>.{2})(?:-(?<country>.{2}))*$/,
+  });
 
   return {
     passthroughFileCopy: true,
---
+++ src/en/en.11tydata.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const i18n = require('eleventy-plugin-i18n-gettext');
+
+module.exports = () => {
+  return i18n.enhance11tydata({}, __dirname);
+};
