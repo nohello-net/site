b1c39f958208226b6c07562d476da79167ae27c7
Joshua May <notjosh@gmail.com>
2022-02-01 11:39:31 +0100

    Added unit tests for locale files

---
+++ test/unit/locales.spec.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+const fs = require('fs');
+
+describe('Locales', () => {
+  const locales = require('../../src/_data/locales.json');
+  const base = locales.find((l) => l.path === 'en');
+  const others = locales.filter((l) => l.path !== base.path);
+
+  it('should have base locale', () => {
+    assert.notEqual(base, null);
+  });
+
+  for (const locale of locales) {
+    describe(`Locale: ${locale.name}`, () => {
+      const messagesPoPath = `locales/${locale.path}/messages.po`;
+
+      describe('messages.po', () => {
+        it('exists', () => {
+          const result = fs.existsSync(messagesPoPath);
+          assert.equal(result, true);
+        });
+      });
+
+      describe('src/<lang>/ folder', () => {
+        it('11tydata.js exists', () => {
+          const path = `src/${locale.path}/${locale.path}.11tydata.js`;
+          const result = fs.existsSync(path);
+          assert.equal(result, true);
+        });
+
+        it('index.njk exists', () => {
+          const path = `src/${locale.path}/index.njk`;
+          const result = fs.existsSync(path);
+          assert.equal(result, true);
+        });
+      });
+
+      describe.only('translation strings', () => {
+        const messagesjs = fs.readFileSync('locales/messages.js', 'utf-8');
+        const messagespo = fs.readFileSync(messagesPoPath, 'utf-8');
+
+        // search for strings that look like: _("key")
+        const keys = [...messagesjs.matchAll(/_\(['"](.*)['"]\)/g)].map(
+          (match) => match[1]
+        );
+
+        for (const key of keys) {
+          it(`contains key: ${key}`, () => {
+            const found = messagespo.match(new RegExp(`msgid "${key}"`, 'g'));
+
+            assert.notEqual(found, null);
+            assert.equal(found.length, 1);
+          });
+        }
+      });
+    });
+  }
+});
