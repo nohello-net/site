5f0afd13895ef9142b46c50aa99982f61dde9533
Joshua May <notjosh@gmail.com>
2022-02-06 00:25:47 +0100

    Added image optimisation step, and delayed the preload

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
 const i18n = require('eleventy-plugin-i18n-gettext');
+const Image = require('@11ty/eleventy-img');
+
+const imagify = async (src, options = {}) => {
+  const defaults = {
+    outputDir: './build/img/',
+  };
+
+  const metadata = await Image(src, { ...defaults, ...options });
+
+  return metadata;
+};
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('src/css');
-  eleventyConfig.addPassthroughCopy('src/img');
   eleventyConfig.addPassthroughCopy('src/js');
 
   eleventyConfig.addPlugin(i18n, {
@@ -19,6 +29,52 @@ module.exports = (eleventyConfig) => {
       autoescape: false,
     });
   }
+
+  eleventyConfig.addNunjucksAsyncShortcode(
+    'image',
+    async (src, alt, widths = [null]) => {
+      const metadata = await imagify(src, { widths });
+
+      const imageAttributes = {
+        alt,
+      };
+
+      return Image.generateHTML(metadata, imageAttributes);
+    }
+  );
+
+  eleventyConfig.addNunjucksAsyncShortcode(
+    'imageURL',
+    async (src, inOptions = {}) => {
+      const defaults = {
+        widths: [null],
+      };
+
+      const outOptions = {
+        ...defaults,
+      };
+
+      if (inOptions.format) {
+        outOptions.formats = [inOptions.format];
+      }
+
+      if (inOptions.animated) {
+        outOptions.sharpOptions = { animated: true };
+      }
+
+      const metadata = await imagify(src, outOptions);
+
+      const keys = Object.keys(metadata);
+
+      if (keys.length !== 1) {
+        throw new Error(
+          `Expected 1 format for imageURL, got ${keys.length} on src: ${src}`
+        );
+      }
+
+      return metadata[keys[0]][0].url;
+    }
+  );
 
   return {
     passthroughFileCopy: true,
