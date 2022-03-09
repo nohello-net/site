aa2617221ef3ae035cfa3769376b73deeff2377a
Joshua May <notjosh@gmail.com>
2022-03-09 21:38:33 +0100

    Updated snapshot with new languages

--- test/ui/locale-detection.spec.ts
+++ test/ui/locale-detection.spec.ts
@@ -40,6 +40,15 @@ test.describe('locale detection', () => {
     });
   });
 
+  test.describe('pt', () => {
+    test.use({ locale: 'pt' });
+    test('base locale', async ({ page }) => {
+      await page.goto(BASE_URL);
+
+      expect(page.url()).toBe(url('en/'));
+    });
+  });
+
   test('remembers override setting', async ({ page }) => {
     await page.goto(BASE_URL);
     expect(page.url()).toBe(url('en/'));
